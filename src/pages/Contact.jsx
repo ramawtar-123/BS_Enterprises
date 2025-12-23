b27e10883deb6a80bcd21854827a8bb6ac4edfd8
ramawtar-123 <149510040+ramawtar-123@users.noreply.github.com>
2025-12-23 15:56:25 +0530

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { motion } from 'framer-motion';
 import { useNavigate } from 'react-router-dom';
-import { Input } from '../components/ui/Input';
+import { Input } from '../Components/ui/Input';
 
 export default function Contact() {
     const navigate = useNavigate();
